dcf1ba36a60faf265317d1560e7c518bff2db5eb
Gabi <gabichmielecka@gabi.local>
2025-03-05 15:27:06 +0100

    funkcjonująca gra 2 ai

--- Project1/easy_ai.py
+++ Project1/easy_ai.py
@@ -1,105 +1,63 @@
 # gra
+from easyAI import TwoPlayerGame, Negamax, AI_Player
 import random
-def main():
-    # Define an empty rocklist to append rocks to, define random integers, call functions
-    board = [1,2,3,4,5]
-    name1, name2 = get_players()
-
-    player = name1  # Set current player to 1 (for switching later)
-
-    get_board(board,  player)  # Set initial board
-
-    play_again(board, name1, name2, player)
 
 
-# Post: takes no arguments and returns the player names as strings entered by the user
-def get_players():
-    return input("Enter player 1 name: "), input("Enter player 2 name: ")
+class Nim(TwoPlayerGame):
+
+    def __init__(self, players=None, piles=(1, 2, 3, 4, 5)):
+        self.players = players
+        self.piles = list(piles)
+        self.current_player = 1  # player 1 starts.
+
+    def possible_moves(self):
+        return [
+            "%d,%d" % (i + 1, j)
+            for i in range(len(self.piles))
+            for j in range(1, self.piles[i]+1)
+        ]
+
+    def make_move(self, move):
+        move = list(map(int, move.split(",")))
+
+        # 10% chance to reduce move[1] by 1
+        if random.random() < 0.1 and move[1] > 0:
+            move[1] -= 1
+
+        self.piles[move[0] - 1] -= move[1]
 
 
+    def show(self):
+        print(" ".join(map(str, self.piles)))
 
-def get_board(board,  player):
-    # Get initial board
-    print("Let's look at the board now.")
-    print("-" * 25)
-    for i in range(len(board)):
-        rocks = board[i]
-        print('Pile {}: {}'.format(i + 1, 'O' * rocks))
-    print("-" * 25)
+    def win(self):
+        if sum(self.piles) == 0:
+            return True
 
+    def is_over(self):
+        return self.win()
 
+    def scoring(self):
+        return 100 if self.win() else 0
 
-def get_valid_input(board, player):
-    # Begin loop that tests for valid input - if valid, break loop - if not, keep asking
-    while True:
-        piles = input(' {}, Pick a pile to remove from: '.format(player))
-        stones = input('{}, how many stones to remove? ')
+def play_ai_vs_ai(num_games=10):
+    ai_algo = Negamax(3)  # Set search depth
+    results = {"AI1": 0, "AI2": 0}
 
-        # If all condiitons for input are CORRECT, break out of loop
-        if (stones and piles) and (stones.isdigit()) and (piles.isdigit()):
-            if (int(stones) > 0) and (int(piles) <= len(board)) and (int(piles) > 0):
-                if (int(stones) <= board[int(piles) - 1]):
-                    if (int(stones) != 0) and (int(piles) != 0):
-                        break
+    for game_num in range(num_games):
+        # Switch starting player every game
+        players = [AI_Player(ai_algo), AI_Player(ai_algo)] if game_num % 2 == 0 else [AI_Player(ai_algo), AI_Player(ai_algo)][::-1]
+        game = Nim(players)
+        game.play()
 
-        # If not, display this statement
-        print("Hmmm. You entered an invalid value. Try again, {}.".format(player))
+        winner = "AI1" if game.current_player == 2 else "AI2"
+        results[winner] += 1
+        print(f"Game {game_num + 1}: {winner} wins!")
 
-    # Update state
-    board[int(piles) - 1] -= int(stones)
-
-    # Keep playing game
-    continue_game(board, player)
+    print("\nFinal Results:")
+    print(f"AI1 Wins: {results['AI1']}")
+    print(f"AI2 Wins: {results['AI2']}")
 
 
-def continue_game(board, player):
-    print("Let's look at the board now.")
-    print("-" * 25)
-    for i in range(len(board)):
-        print("Pile {}: {}".format(i + 1, 'O' * board[i]))
-
-    print("-" * 25)
-
-    # In the case when game is over, do not display computer hint for empty board
-    # if rockList != [0] * len(rockList):
-    #     nim_sum(rockList, randPile)
-
-    # print(rockList)
-
-
-def play_again(board, name1, name2, player):
-    # Begin loop to initiate player switching
-    while True:
-        get_valid_input(board, player)
-
-        # To determine winner, check if rockList contains all 0's on that player's turn
-        if sum(board) == 1:
-            print("{} is the winner of this round!".format(name2 if player == name1 else name1))
-            break
-        #     user = input("Do you want to play again? Enter y for yes, anything for no: ")
-        #
-        #     if user.lower() == 'y':
-        #         # reset all conditions, start the game again
-        #         rockList = []
-        #         randPile = random.randint(2, 5)
-        #         name1, name2 = get_players()
-        #         player = name1
-        #         get_board(rockList, randPile, randRock, player)
-        #         get_valid_input(rockList, randPile, player)
-        #
-        #     else:
-        #         break
-        #
-        # switch players 2->1, 1->2
-        if player == name1:
-            player = name2
-
-        else:
-            player = name1
-
-
-
-
-main()
-
-#uzytkownicy ai
+if __name__ == "__main__":
+    play_ai_vs_ai(10)  # Run 10 AI vs AI games
